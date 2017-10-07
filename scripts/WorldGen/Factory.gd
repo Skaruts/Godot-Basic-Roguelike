f6290558d9c44ef3ad1fe8f0139630aca3e7cd49
Skaruts <skaruts@users.noreply.github.com>
2017-10-07 03:05:58 +0100

    Update Factory.gd

--- scripts/WorldGen/Factory.gd
+++ scripts/WorldGen/Factory.gd
@@ -27,5 +27,8 @@ func set_seed(custom_seed = null):
 
 	# then use the new seed
 	seed(SEED)
+	print("-------------------------------------------------")
 	print("Factory.set_seed: | seed: ", SEED)
+	print("-------------------------------------------------")
+	print("\n\n")
 
