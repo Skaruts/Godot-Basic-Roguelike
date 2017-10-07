98d830c185868ffa48e09e5c7372428c63ade69c
Skaruts <skaruts@users.noreply.github.com>
2017-10-07 05:28:49 +0100

    Ditched tilemap as dictionary with Vector2 keys
    Couldn't figure out how to sort the keys.

--- scripts/WorldGen/BSP_Factory/BSP_Dungeon.gd
+++ scripts/WorldGen/BSP_Factory/BSP_Dungeon.gd
@@ -6,9 +6,9 @@ var use_bresenham = false 	# use Bresenham or RedBlobGames line algorithm
 var pre_corpse = preload("res://scenes/entities/Corpse.tscn")
 var pre_tile = preload("res://scripts/Tile.gd")
 
-var tilemap = {}	# array of Tile objects
-var fovmap			# field of view map - array of bytes (RawArray)
-var clear_fovmap	# same as fovmap, but stays clear (so I don't need to keep recreating the fovmap)
+var tilemap = []	# array of Tile objects
+var fovmap	= Array()		# field of view map - array of bytes (RawArray)
+var clear_fovmap = Array()	# same as fovmap, but stays clear (so I don't need to keep recreating the fovmap)
 var leaves 	= []	# BSP Leaves
 var rooms 	= []	# Rooms
 var halls 	= []	# Hallways
@@ -27,8 +27,8 @@ func _init(w, h):
 func build_map( map ):
 	if tilemap.size(): clear_map()
 	w = int(w)
-	for i in range(h):
-		for j in range(w):
+	for j in range(h):
+		for i in range(w):
 			var tile = pre_tile.new()
 
 			if map[i+j*w] in [RFLOOR_ID, HFLOOR_ID]:
@@ -41,18 +41,17 @@ func build_map( map ):
 
 			# TODO: make doors
 
-			tilemap[ Vector2(j, i) ] = tile
+			tilemap.append( tile )
 			add_child( tile )
 			tile.set_owner( self )
 
 	create_fovmap()		# REDUNDANT
 
 func create_fovmap():
-	clear_fovmap = RawArray()
 	for i in range( w*h ):
 		clear_fovmap.append( 0 )
 
-	fovmap = clear_fovmap
+	fovmap = Array(clear_fovmap)
 
 # calculate visibility map based on player location
 func calc_fovmap(pos, vis_range):
@@ -60,39 +59,44 @@ func calc_fovmap(pos, vis_range):
 	var RIGHT	= clamp( pos.x+vis_range, 0, w )
 	var TOP		= clamp( pos.y-vis_range, 0, h )
 	var BOTTOM	= clamp( pos.y+vis_range, 0, h )
+	var visw = RIGHT - LEFT
+	var vish = BOTTOM - TOP
+	var dunw = w
 
-	fovmap = clear_fovmap
+	print( "left: ", LEFT, "right: ", RIGHT, "R - L: ", RIGHT - LEFT )
+
+	fovmap = Array(clear_fovmap)
 
 	for j in range( TOP, BOTTOM ):
 		for i in range( LEFT, RIGHT ):
 			var points = utils.line(pos, Vector2(i, j), use_bresenham)
 			fovmap[i+j*w] = 1
 			for p in points:
-				if tilemap[ Vector2(p.y, p.x) ].is_solid:
+				# print ("[", p.x, ", ", p.y, "], ")
+				# print ("[", p.x + p.y * w, "], ")
+				if tilemap[ p.x+p.y*w ].is_solid:
 					fovmap[i+j*w] = 0
 					break
+
 
 	# make walls visible
 	for j in range( TOP, BOTTOM ):
 		for i in range( LEFT, RIGHT ):
 			# if it's a wall, that isn't visible, and is next to a visible/revealed floor
-			if tilemap[ Vector2(j, i) ].is_solid			\
+			if tilemap[ i+j*w ].is_solid			\
 			and not fovmap[i+j*w]				\
 			and has_neighboring_floor(i, j):
 				fovmap[i+j*w] = 1                # then show it too
 
-			tilemap[ Vector2(j, i) ].set_visible(fovmap[i+j*w])
+			tilemap[ i+j*w ].set_visible(fovmap[i+j*w])
 
 	# make the tiles visible
 	for j in range( h ):
 		for i in range( w ):
-			# THIS CAN PROBABLY BE MERGED WITH THE LOOP ABOVE
-			# THIS CAN PROBABLY BE MERGED WITH THE LOOP ABOVE
-			# THIS CAN PROBABLY BE MERGED WITH THE LOOP ABOVE
 			if not show_all_tiles:
-				tilemap[ Vector2(j, i) ].set_visible(fovmap[i+j*w])
+				tilemap[ i+j*w ].set_visible(fovmap[i+j*w])
 			else:
-				tilemap[ Vector2(j, i) ].set_visible(1)
+				tilemap[ i+j*w ].set_visible(1)
 
 func has_neighboring_floor(x, y):
 	if is_visible_floor(x-1, y-1)	\
@@ -108,7 +112,7 @@ func has_neighboring_floor(x, y):
 
 func is_visible_floor(x, y):
     if (x > 0) and (x < MW) and (y > 0) and (y < MH):
-        if not tilemap[ Vector2(y, x) ].is_solid	\
+        if not tilemap[ x+y*w ].is_solid	\
         and fovmap[x+y*w]:
             return true
     return false
@@ -127,7 +131,7 @@ func take_turn(player):
 		e.set_visible( fovmap[e.pos.x + e.pos.y*w] )
 
 func can_walk(x, y):
-	return not tilemap[ Vector2(y, x) ].is_solid
+	return not tilemap[ x+y*w ].is_solid
 
 func check_for_mob(x, y):
 	for m in mobs:
@@ -154,22 +158,5 @@ func add_corpse(cname, pos):
 func clear_map():
 	for t in tilemap:
 		t.get_parent().remove_child(t)
-		t.free()
-	tilemap.clear()
-
-#func switch_texture():
-#	var TS = textures.get_tile_size()
-#
-#	for j in range( tilemap.size() ):
-#		for i in range( tilemap[0].size() ):
-#			tilemap[i+j*w].switch_texture()
-#			#tilemap[i+j*w].set_global_pos( Vector2(i, j) )
-#
-#	for m in mobs:		m.switch_texture()
-#	for c in corpses:	c.switch_texture()
-
-
-
-#func finalize():
-#	for l in leaves():
-#		add_child(l)
+		t.queue_free()
+	tilemap = []
